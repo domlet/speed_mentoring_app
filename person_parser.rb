377c7d3942b6db60c1cc0e7adb322673693074d5
Frank Aiello <frankvaiello@gmail.com>
2016-11-09 08:03:47 -0800

    Changes return value for parser to output array of person hashes

--- person_parser.rb
+++ person_parser.rb
@@ -16,18 +16,22 @@ class PersonParser
       @people = parse_people_objects_from_file
     end
     pp @people
-    p '*' * 50
-    @people.flatten
+    # p '*' * 50
+    # @people.flatten
   end
 
   private
   def parse_people_objects_from_file
-    file = CSV.read(@file, :quote_char => "|", :col_sep=>',')
-    keys, values = file.first, file[1..-1]
-    puts keys
-    # new_keys = keys.map {|i| i.to_sym}
-    # puts new_keys
-    # @people = values.map {|data| new_keys.zip(data)}.map(&:to_h)
+    people = []
+    CSV.foreach("people_fake_data.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
+      people << Hash[row.headers[0..-1].zip(row.fields[0..-1])]
+    end
+    people
+    # file = CSV.read(@file, :quote_char => "|", :col_sep=>',')
+    # keys, values = file.first, file[1..-1]
+    # keys = keys.map {|i| i.to_sym}
+    # people = values.map {|data| keys.zip(data)}.map(&:to_h)
+    # pp keys
     # @people.map {|data| data[:timestamp] = DateTime.parse(data[:timestamp])}
     # @people.map! {|data| [Person.new(data)]}
     # @people
@@ -36,16 +40,16 @@ end
 
 
 # frank driver code
-# test = PersonParser.new('people_fake_data.csv')
-# p test.people[0]
+test = PersonParser.new('people_fake_data.csv')
+test.people
 
 # dom driver code
 
-people = {}
-# DRIVER
-    CSV.foreach("people_fake_data.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
-      people[row.fields[0]] = Hash[row.headers[0..-1].zip(row.fields[0..-1])]
-    end
+# people = {}
+# # DRIVER
+#     CSV.foreach("people_fake_data.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
+#       people[row.fields[0]] = Hash[row.headers[0..-1].zip(row.fields[0..-1])]
+#     end
 
 
-pp people
+# pp people
