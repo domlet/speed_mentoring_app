4fb742913ec44508a1780e1493b9c2587831942f
Frank Aiello <frankvaiello@gmail.com>
2016-11-09 08:15:18 -0800

    Adds method in parser class to create new person objects

--- person_parser.rb
+++ person_parser.rb
@@ -13,35 +13,30 @@ class PersonParser
     if @people
       return @people
     else
-      @people = parse_people_objects_from_file
+      @people = parse_people_from_file
     end
-    pp @people
-    # p '*' * 50
-    # @people.flatten
+  end
+
+  def create_people_objects
+    people.map! {|person| Person.new(person)}
   end
 
   private
-  def parse_people_objects_from_file
+
+  def parse_people_from_file
     people = []
-    CSV.foreach("people_fake_data.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
+    CSV.foreach(@file, :headers => true, :header_converters => :symbol, :converters => :all) do |row|
       people << Hash[row.headers[0..-1].zip(row.fields[0..-1])]
     end
     people
-    # file = CSV.read(@file, :quote_char => "|", :col_sep=>',')
-    # keys, values = file.first, file[1..-1]
-    # keys = keys.map {|i| i.to_sym}
-    # people = values.map {|data| keys.zip(data)}.map(&:to_h)
-    # pp keys
-    # @people.map {|data| data[:timestamp] = DateTime.parse(data[:timestamp])}
-    # @people.map! {|data| [Person.new(data)]}
-    # @people
   end
+
 end
 
 
 # frank driver code
 test = PersonParser.new('people_fake_data.csv')
-test.people
+pp test.create_people_objects
 
 # dom driver code
 
