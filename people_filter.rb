f7840c5bc0c37c63825c4aa848e7b5d178473493
Frank Aiello <frankvaiello@gmail.com>
2016-11-09 09:46:21 -0800

    Refactor people filter to have separate method for people with both mentor/mentee

--- people_filter.rb
+++ people_filter.rb
@@ -1,14 +1,14 @@
 module PeopleFilter
 
   def is_mentor?
-    self.would_like_to_be_a == "mentor"
+    self.would_like_to_be_a == "mentor" || self.would_like_to_be_a == "both"
   end
 
   def is_mentee?
-    self.would_like_to_be_a == "mentee"
+    self.would_like_to_be_a == "mentee" || self.would_like_to_be_a == "both"
   end
 
-  def is_both?
+  def is_mentor_and_mentee?
     self.would_like_to_be_a == "both"
   end
 
