9e54dd86022893274bda189c00d0cfbc757b1060
Frank Aiello <frankvaiello@gmail.com>
2016-11-12 14:25:28 -0800

    commit to test GitHub ssh prompt on Frank's mac

--- matcher.rb
+++ matcher.rb
@@ -17,8 +17,15 @@ class Matcher
     @people.select {|person| person.is_mentee?}
   end
 
+  # fix method so that it assigns value to matches (should be an Array)
+  # Not returning the right size of mentors or matches
   def pair_mentors_with_mentees
-    mentors.each {|mentor| mentor.matches = mentees}
+    puts mentors.length
+    mentors.each do |mentor|
+      mentor.matches = mentees
+      puts mentor.first_name
+      puts mentor.matches.length
+    end
   end
 
   def pair_mentees_with_mentors
@@ -27,7 +34,9 @@ class Matcher
 
   def match_times_available
     people.each do |person|
-       pp person.can_attend_mentoring_sessions
+      person.matches = person.matches.select do |match|
+        match.can_attend_mentoring_sessions.any? {|time| person.can_attend_mentoring_sessions.include?(time) }
+      end
     end
   end
 
@@ -35,16 +44,33 @@ end
 
 # Driver Code
 test_people = PersonParser.new('people_fake_data.csv')
-matcher = Matcher.new(test_people.people_objects)
+people = test_people.people_objects
+matcher = Matcher.new(people)
+# puts matcher.people
 matcher.pair_mentors_with_mentees
 matcher.pair_mentees_with_mentors
-matcher.match_times_available
+# matcher.match_times_available
 
-# pp matcher.people[0].times_available
+# Only some people get matched and other do not
+
+puts matcher.people[0].first_name
+puts matcher.people[0].matches
 
 # Test return value for pairing methods
-# puts matcher.mentees[0].matches
-# puts "*" * 20
-# puts matcher.mentors[0].matches
+puts "#{matcher.people.length} people are available out of #{people.length}"
+# should be 12 mentees and 13 mentors
+# around seven people put both
+puts "Out of #{matcher.mentees.length} matches, these match for #{matcher.mentees[2].first_name}\n\n"
+
+matcher.people[2].matches.each do |match|
+  puts match.first_name
+end
+puts "*" * 22
+
+puts "Out of #{matcher.mentors.length} matches, these match for #{matcher.mentors[2].first_name}\n\n"
+
+matcher.people[2].matches.each do |match|
+  puts match.first_name
+end
 
 
